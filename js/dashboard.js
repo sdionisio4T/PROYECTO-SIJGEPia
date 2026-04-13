c1f8aa28de642b02c814ffbe55bd75d786a37c95
Daniel Dionisio <sdionisio4@gmail.com>
2026-04-13 12:41:58 -0500

    Prueba eliminada

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,14 +4,6 @@
 
 cargarUsuarioHeader()
 
-// PRUEBA - borrar después
-confirmar("¿Estás seguro de eliminar este expediente?").then(function(respuesta) {
-  if (respuesta) {
-    mostrarExito("Expediente eliminado correctamente")
-  } else {
-    mostrarError("Operación cancelada")
-  }
-})
 
 
 document.querySelector(".logout").addEventListener("click", function() {
